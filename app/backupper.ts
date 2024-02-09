9f1fbd06faa088e6500b8833eba14784ee608e02
vinigiu <viniciusgiuseppe1992@gmail.com>
2024-02-09 09:38:29 -0300

    first commit

---
+++ app/backupper.ts
@@ -0,0 +1,37 @@
+import child_process from 'child_process';
+import path from 'path';
+import { dbConfigurations } from './dbConfigurations';
+
+const doBackup = async (dbConfigurations: any) => {
+    for (let dbIndex = 0; dbIndex < dbConfigurations.length; dbIndex++) {
+        const config = dbConfigurations[dbIndex];
+        const timestamp = new Date().toISOString().replace(/:/g, '-');
+        
+        let dbName = config.database;
+        
+        if (config.database === 'defaultdb') {
+            dbName = 'accdb';
+        }
+
+        const backupFilename = `${dbName}_${timestamp}.bkp`;
+
+        const outputFilePath = path.join(__dirname, `../files/${backupFilename}`)
+      
+        const command = `PGPASSWORD="${config.password}" pg_dump -U ${config.user} -h ${config.host} -p ${config.port} ${config.database} > ${outputFilePath}`;
+      
+        try {
+          console.log(`Iniciando o backup do banco de dados ${dbName}...`);
+          child_process.execSync(command, { stdio: 'inherit' });
+          console.log(`Backup do banco de dados ${dbName} concluído com sucesso.`);
+        } catch (error: any) {
+          console.error(`Erro ao fazer backup do banco de dados ${dbName}: ${error.message}`);
+        }
+      }
+};
+
+// Agendamento do backup (executa toda sexta-feira às 09h30)
+// cron.schedule('30 9 * * 5', () => {
+//   for (const config of dbConfigurations) {
+    doBackup(dbConfigurations);
+//   }
+// });
---
+++ app/dbConfigurations.ts
@@ -0,0 +1,51 @@
+export const dbConfigurations = [
+  {
+    host: process.env.TRIAL_HOST,
+    user: process.env.TRIAL_USER,
+    database: process.env.TRIAL_DB,
+    password: process.env.TRIAL_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.CCR_HOST,
+    user: process.env.CCR_USER,
+    database: process.env.CCR_DB,
+    password: process.env.CCR_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.ACC_HOST,
+    user: process.env.ACC_USER,
+    database: process.env.ACC_DB,
+    password: process.env.ACC_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.FERBASA_HOST,
+    user: process.env.FERBASA_USER,
+    database: process.env.FERBASA_DB,
+    password: process.env.FERBASA_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.VOTORANTIM_HOST,
+    user: process.env.VOTORANTIM_USER,
+    database: process.env.VOTORANTIM_DB,
+    password: process.env.VOTORANTIM_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.PARKER_HOST,
+    user: process.env.PARKER_USER,
+    database: process.env.PARKER_DB,
+    password: process.env.PARKER_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+  {
+    host: process.env.MERCURIO_HOST,
+    user: process.env.MERCURIO_USER,
+    database: process.env.MERCURIO_DB,
+    password: process.env.MERCURIO_PASSWORD,
+    port: process.env.DEFAULT_PORT,
+  },
+];
